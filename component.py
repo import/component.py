d4831268819a12c5607eecd6634235ff5607793b
Daniel Chatfield <chatfielddaniel@gmail.com>
2013-07-30 12:32:37 +0100

    Add component header

---
+++ component.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    component
+    ~~~~~~~~~
+
+    A module that implements various helper functions to assist in using
+component(1) with a python workflow.
+
+    :copyright: (c) 2013 by Daniel Chatfield
+    :license: MIT, see LICENSE for details.
+"""
